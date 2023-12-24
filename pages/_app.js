cfa8ec53e9bb1d1a500fa80aea31c4071406e4d8
Yogesh bhoye <bhoyeyogesh0202@gmail.com>
2023-12-24 22:40:29 +0530

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { Provider } from "react-redux";
+import store from "@/store/store";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Online Shoes Store</title>
+      </Head>
+      <Provider store={store}>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </Provider>
+    </>
+  );
 }
---
+++ utils/urls.js
@@ -0,0 +1,5 @@
+const STRAPI_API_TOKEN = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:1337";
+
+export { STRAPI_API_TOKEN, API_URL };
